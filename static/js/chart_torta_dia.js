4ec00fe4b5d031497ec77079264902011ddc301b
Jose Eduardo Laruta Espejo <eduardo.laruta@gmail.com>
2015-06-07 02:00:02 -0400

    creado js para la torta del resumen diario

---
+++ static/js/chart_torta_dia.js
@@ -0,0 +1,59 @@
+$(function () {
+            $(document).ready(function () {
+                var chart_options = {
+                    chart: {
+                        renderTo: "resumen_dia",
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'Resumen de consumo por áreas'
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.y:.1f} KWh</b> - {point.percentage:.1f}%'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Consumo de Potencia',
+                        data: [
+                            ['Firefox',   45.0],
+                            ['IE',       26.8],
+                            {
+                                name: 'Chrome',
+                                y: 12.8,
+                                sliced: true,
+                                selected: true
+                            },
+                            ['Safari',    8.5],
+                            ['Opera',     6.2],
+                            ['Others',   0.7]
+                        ]
+                    }]
+                };
+                var data_url;
+                // Build the chart
+
+                $("#btn_buscar").click(function(){      //cuando presionamos boton
+                    fecha = $("input#dia").val()
+                    data_url = '/rango/torta_dia_json/?dia=' + fecha;
+                    $("#jquery_test1").text(data_url);
+                    $.getJSON(data_url, function(data){
+                        chart_options.series[0] = data;
+                        var chart = new Highcharts.Chart(chart_options);
+                    });
+                });
+
+            });
+
+        });
