ce366b14f884000c361390eefa2d85d4379a0f73
Jose Eduardo Laruta Espejo <eduardo.laruta@gmail.com>
2015-06-10 00:48:18 -0400

    modificado para recibir datos del servidor

--- static/js/stock_med.js
+++ static/js/stock_med.js
@@ -1,5 +1,10 @@
 $(function () {
-    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-v.json&callback=?', function (data) {
+
+    var medidor_slug = $("#med_name").text();
+    //variable que almacena el url de la vista que nos retornara los datos
+    var data_url;
+    data_url = '/rango/completo_json/?medidor_slug='+medidor_slug;
+    $.getJSON(data_url, function (data) {
 
         // create the chart
         $('#chart_medidor').highcharts('StockChart', {
@@ -12,23 +17,96 @@ $(function () {
             },
 
             title: {
-                text: 'AAPL Stock Volume'
+                text: 'Histórico de Consumo'
             },
+
+            subtitle: {
+                text: "Sector: "+ medidor_slug,
+                x: -20
+            },
+
+            yAxis: [{ // Primary yAxis
+                labels: {
+                    format: '{value}Kwh',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'Promedio',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                min: 0,
+                max:50
+
+            }, { // Secondary yAxis
+                title: {
+                    text: 'Acumulado',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels: {
+                    format: '{value} Kwh',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                max: 8000
+                //opposite: true
+            }],
 
             series: [{
                 type: 'column',
-                name: 'AAPL Stock Volume',
+                name: 'Potencia',
+                data: data,
+                yAxis: 1,
+                dataGrouping: {
+                    //forced: true,
+                    units: [
+                        [
+                            'week',
+                            [1]
+                        ],
+                        
+                        [
+                            'month', // unit name
+                            [1] // allowed multiples
+                        ], 
+                        [
+                            'hour',
+                            [1]
+                        ]
+                    ]
+                }
+            },
+            {
+                type: 'line',
+                name: 'Promedio',
                 data: data,
                 dataGrouping: {
-                    units: [[
-                        'week', // unit name
-                        [1] // allowed multiples
-                    ], [
-                        'month',
-                        [1, 2, 3, 4, 6]
-                    ]]
+                    //forced: true,
+                    units: [
+                        
+                        [
+                            'week',
+                            [1]
+                        ],
+                        
+                        [
+                            'month', // unit name
+                            [1] // allowed multiples
+                        ], 
+                        [
+                            'hour',
+                            [1]
+                        ]
+                    ]
                 }
-            }]
+            }],
+
         });
     });
 });
